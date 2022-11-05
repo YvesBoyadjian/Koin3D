a098ec39d923639c6447e27a92f954925334bd87
Yves BOYAdjian <yboyadjian@hotmail.fr>
2022-11-05 20:53:39 +0100

    SbMatrixd

--- jscenegraph/src/jscenegraph/database/inventor/SbMatrixd.java
+++ jscenegraph/src/jscenegraph/database/inventor/SbMatrixd.java
@@ -19,55 +19,58 @@ public class SbMatrixd implements Mutable {
 	  public SbMatrixd() { }
 
 
-//
-// Constructor from a 4x4 array of elements
-//
+    //
+    // Constructor from a 4x4 array of elements
+    //
 
-public SbMatrixd(final double[][] m)
-{
-    matrix[0][0] = m[0][0];
-    matrix[0][1] = m[0][1];
-    matrix[0][2] = m[0][2];
-    matrix[0][3] = m[0][3];
-    matrix[1][0] = m[1][0];
-    matrix[1][1] = m[1][1];
-    matrix[1][2] = m[1][2];
-    matrix[1][3] = m[1][3];
-    matrix[2][0] = m[2][0];
-    matrix[2][1] = m[2][1];
-    matrix[2][2] = m[2][2];
-    matrix[2][3] = m[2][3];
-    matrix[3][0] = m[3][0];
-    matrix[3][1] = m[3][1];
-    matrix[3][2] = m[3][2];
-    matrix[3][3] = m[3][3];
-}
+    public SbMatrixd(final double[][] m)
+    {
+        matrix[0][0] = m[0][0];
+        matrix[0][1] = m[0][1];
+        matrix[0][2] = m[0][2];
+        matrix[0][3] = m[0][3];
+        matrix[1][0] = m[1][0];
+        matrix[1][1] = m[1][1];
+        matrix[1][2] = m[1][2];
+        matrix[1][3] = m[1][3];
+        matrix[2][0] = m[2][0];
+        matrix[2][1] = m[2][1];
+        matrix[2][2] = m[2][2];
+        matrix[2][3] = m[2][3];
+        matrix[3][0] = m[3][0];
+        matrix[3][1] = m[3][1];
+        matrix[3][2] = m[3][2];
+        matrix[3][3] = m[3][3];
+    }
 
-	
+    // java port
+    public void copyFrom(final double[][] m) {
+        matrix[0][0] = m[0][0];
+        matrix[0][1] = m[0][1];
+        matrix[0][2] = m[0][2];
+        matrix[0][3] = m[0][3];
+        matrix[1][0] = m[1][0];
+        matrix[1][1] = m[1][1];
+        matrix[1][2] = m[1][2];
+        matrix[1][3] = m[1][3];
+        matrix[2][0] = m[2][0];
+        matrix[2][1] = m[2][1];
+        matrix[2][2] = m[2][2];
+        matrix[2][3] = m[2][3];
+        matrix[3][0] = m[3][0];
+        matrix[3][1] = m[3][1];
+        matrix[3][2] = m[3][2];
+        matrix[3][3] = m[3][3];
+    }
 
-	/* (non-Javadoc)
-	 * @see jscenegraph.port.Mutable#copyFrom(java.lang.Object)
-	 */
-	@Override
-	public void copyFrom(Object other) {
-		final double[][] m = ((SbMatrixd)other).getValue();
-	    matrix[0][0] = m[0][0];
-	    matrix[0][1] = m[0][1];
-	    matrix[0][2] = m[0][2];
-	    matrix[0][3] = m[0][3];
-	    matrix[1][0] = m[1][0];
-	    matrix[1][1] = m[1][1];
-	    matrix[1][2] = m[1][2];
-	    matrix[1][3] = m[1][3];
-	    matrix[2][0] = m[2][0];
-	    matrix[2][1] = m[2][1];
-	    matrix[2][2] = m[2][2];
-	    matrix[2][3] = m[2][3];
-	    matrix[3][0] = m[3][0];
-	    matrix[3][1] = m[3][1];
-	    matrix[3][2] = m[3][2];
-	    matrix[3][3] = m[3][3];
-	}
+    /* (non-Javadoc)
+     * @see jscenegraph.port.Mutable#copyFrom(java.lang.Object)
+     */
+    @Override
+    public void copyFrom(Object other) {
+        SbMatrixd sbMatrix = (SbMatrixd) other;
+        copyFrom(sbMatrix.matrix);
+    }
 
     //! Sets matrix to rotate by given rotation.
     public void        setRotate(final SbRotationd rotation) {
